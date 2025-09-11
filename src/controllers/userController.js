a6b9202d88877576b61d89e98277f199bfc5d2dc
SarthakKotewale <sarthakk0808@gmail.com>
2025-09-11 21:14:38 +0530

    feat: completed user apis

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -181,8 +181,8 @@ const toggleFollowArtist = async (req, res) => {
   }
 }
 
-const toggleFollowPlaylist = async(req, res) => {
-  try{
+const toggleFollowPlaylist = async (req, res) => {
+  try {
     const playlistId = req.params.id
     const user = await User.findById(req.user._id)
     const playlist = await Playlist.findById(playlistId)
@@ -196,12 +196,12 @@ const toggleFollowPlaylist = async(req, res) => {
       throw new Error("Playlist not found")
     }
     const playlistIndex = user.followedPlaylists.indexOf(playlistId)
-    if(playlistIndex === -1){
+    if (playlistIndex === -1) {
       user.followedPlaylists.push(playlistId)
       playlist.followers += 1
-    }else{
+    } else {
       user.followedPlaylists.splice(playlistIndex, 1)
-      if(playlist.followers > 0){
+      if (playlist.followers > 0) {
         playlist.followers -= 1
       }
     }
@@ -210,119 +210,11 @@ const toggleFollowPlaylist = async(req, res) => {
       followedPlaylists: user.followedPlaylists,
       message: playlistIndx === -1 ? "Playlist followed" : "Playlist unfollowed",
     });
-  }catch(error){
+  } catch (error) {
     console.error("Error in toggleFollowPlaylist ", error)
     res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ message: "Something went wrong", error: error.message })
 
   }
 }
 
-module.exports = { register, login, getUserProfile, updateUserProfile, toggleLikeSong, toggleFollowArtist, toggleFollowPlaylist};
-
-//============================================================
-
-// const toggleLikeSong = asyncHandler(async (req, res) => {
-//   const songId = req.params.id;
-//   const user = await User.findById(req.user._id);
-//   if (!user) {
-//     res.status(StatusCodes.NOT_FOUND);
-//     throw new Error("User Not Found");
-//   }
-//   const song = await Song.findById(songId);
-//   if (!song) {
-//     res.status(StatusCodes.NOT_FOUND);
-//     throw new Error("User Not Found");
-//   }
-//   const songIndex = user.likedSongs.indexOf(songId);
-//   if (songIndex === -1) {
-//     user.likedSongs.push(songId);
-//     song.likes += 1;
-//   } else {
-//     user.likedSongs.splice(songIndex, 1);
-//     if (song.likes > 0) {
-//       song.likes -= 1;
-//     }
-//   }
-//   await Promise.all([user.save(), song.save()]);
-//   res.status(StatusCodes.OK).json({
-//     likedSongs: user.likedSongs,
-//     message:
-//       songIndex === -1
-//         ? "Song added to liked songs"
-//         : "Song removed from liked songs",
-//   });
-// });
-
-// const toggleFollowArtist = asyncHandler(async (req, res) => {
-//   const artistId = req.params.id;
-//   const user = await User.findById(req.user._id);
-
-//   if (!user) {
-//     res.status(StatusCodes.NOT_FOUND);
-//     throw new Error("User not found");
-//   }
-
-//   const artist = await Artist.findById(artistId);
-//   if (!artist) {
-//     res.status(StatusCodes.NOT_FOUND);
-//     throw new Error("Artist not found");
-//   }
-//   const artistIndex = user.followedArtists.indexOf(artistId);
-
-//   if (artistIndex === -1) {
-//     user.followedArtists.push(artistId);
-//     artist.followers += 1;
-//   } else {
-//     user.followedArtists.splice(artistIndex, 1);
-//     if (artist.followers > 0) {
-//       artist.followers -= 1;
-//     }
-//   }
-//   await Promise.all([user.save(), artist.save()]);
-//   res.status(StatusCodes.OK).json({
-//     followedArtists: user.followedArtists,
-//     message: artistIndex === -1 ? "Artist followed" : "Artist unfollowed",
-//   });
-// });
-
-// const toggleFollowPlaylist = asyncHandler(async (req, res) => {
-//   const playlistId = req.params.id;
-//   const user = await User.findById(req.user._id);
-//   if (!user) {
-//     res.status(StatusCodes.NOT_FOUND);
-//     throw new Error("User not found");
-//   }
-
-//   const playlist = await Playlist.findById(playlistId);
-//   if (!playlist) {
-//     res.status(StatusCodes.NOT_FOUND);
-//     throw new Error("Playlist not found");
-//   }
-//   const playlistIndx = user.followedPlaylists.indexOf(playlistId);
-
-//   if (playlistIndx === -1) {
-//     user.followedPlaylists.push(playlistId);
-//     playlist.followers += 1;
-//   } else {
-//     user.followedPlaylists.splice(playlistIndx, 1);
-//     if (playlist.followers > 0) {
-//       playlist.followers -= 1;
-//     }
-//   }
-//   await Promise.all([user.save(), playlist.save()]);
-//   res.status(StatusCodes.OK).json({
-//     followedPlaylists: user.followedPlaylists,
-//     message: playlistIndx === -1 ? "Playlist followed" : "Playlist unfollowed",
-//   });
-// });
-
-// // const getUsers = asyncHandler(async (req, res) => {});
-// module.exports = {
-//   registerUser,
-//   loginUser,
-//   getUserProfile,
-//   updateUserProfile,
-//   toggleLikeSong,
-//   toggleFollowArtist,
-//   toggleFollowPlaylist,
-// };
+module.exports = { register, login, getUserProfile, updateUserProfile, toggleLikeSong, toggleFollowArtist, toggleFollowPlaylist };
